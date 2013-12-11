c3da20cd2e93289ab0fef8fd6be5c09880303f8a
Simon Matic Langford <simon@exemel.co.uk>
2013-12-11 21:28:13 +0000

    Update publish-doco.sh

--- publish-doco.sh
+++ publish-doco.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/betfair/gh-pages-publishing.git
 cd gh-pages-publishing
-./publish.sh $TRAVIS_REPO_SLUG $TRAVIS_BRANCH $PUBLISH_USERNAME
+sh -x ./publish.sh $TRAVIS_REPO_SLUG $TRAVIS_BRANCH $PUBLISH_USERNAME
