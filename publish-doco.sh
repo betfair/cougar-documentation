46bdb05d7dffeaf28c903b46e08219e83fac8b57
Simon Matic Langford <simon@exemel.co.uk>
2013-12-13 06:40:30 +0000

    Use local publish.sh

--- publish-doco.sh
+++ publish-doco.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/betfair/gh-pages-publishing.git
-cd gh-pages-publishing
+#git clone https://github.com/betfair/gh-pages-publishing.git
+#cd gh-pages-publishing
 bash -x ./publish.sh $TRAVIS_REPO_SLUG $TRAVIS_BRANCH $PUBLISH_USERNAME
